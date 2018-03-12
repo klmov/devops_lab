aaa662878b1bb3a0d18a2aa192f8dec573e76959
Kanstantsin Klimov <klimovkostya5@gmail.com>
2018-03-12 18:31:45 +0300

    Update H2_task4.py

--- H2_task4.py
+++ H2_task4.py
@@ -7,11 +7,11 @@ def word_order(n):
     dict1 = OrderedDict()
     for i in range(n):
         words = input()  # Words
-        if words not in dict.keys():
+        if words not in dict1.keys():
             dict1.update({words: 1})
             continue
         dict1[words] += 1
 
-    print(len(dict.keys()))
+    print(len(dict1.keys()))
     print(*dict1.values())
 word_order(n)
